30cc2c3c2b1adedbf062bfc51657dd94ad6a908a
Nico <niicoporras@gmail.com>
2022-08-31 23:23:55 -0600

    ac

--- INSERTION_SORT.cpp
+++ INSERTION_SORT.cpp
@@ -1,22 +1,17 @@
-// C++ program for insertion sort
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to sort an array using
-// insertion sort
 void insertionSort(int arr[], int n)
 {
+    int PagedArray();
 	int i, key, j;
 	for (i = 1; i < n; i++)
 	{
 		key = arr[i];
 		j = i - 1;
 
-		// Move elements of arr[0..i-1],
-		// that are greater than key, to one
-		// position ahead of their
-		// current position
+
 		while (j >= 0 && arr[j] > key)
 		{
 			arr[j + 1] = arr[j];
@@ -26,8 +21,7 @@ void insertionSort(int arr[], int n)
 	}
 }
 
-// A utility function to print an array
-// of size n
+
 void printArray(int arr[], int n)
 {
 	int i;
@@ -36,10 +30,10 @@ void printArray(int arr[], int n)
 	cout << endl;
 }
 
-// Driver code
 int main()
 {
-	int arr[] = { 12, 11, 13, 5, 6 };
+    int PagedArray();
+	int arr[0] = PagedArray();
 	int N = sizeof(arr) / sizeof(arr[0]);
 
 	insertionSort(arr, N);
@@ -47,4 +41,4 @@ int main()
 
 	return 0;
 }
-// This is code is contributed by rathbhupendra
+
